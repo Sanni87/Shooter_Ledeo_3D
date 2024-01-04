2153d4c5ca57116f4a2089c2d3db282c11a3cb4e
Sanni87 <asier.sannicolas@gmail.com>
2024-01-04 19:23:31 +0100

    Menu scene settings

--- assets/scripts/menu.gd
+++ assets/scripts/menu.gd
@@ -3,17 +3,16 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	%Start.grab_focus()
 
 
 func _on_start_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
 
 
 func _on_exit_pressed():
-	pass # Replace with function body.
+	$Fade/AnimationPlayer.play("fade_out")
+
+func _on_animation_player_animation_finished(anim_name):
+	match anim_name:
+		"fade_out": get_tree().quit()
